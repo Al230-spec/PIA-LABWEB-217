28dcbd150902f27d3a6e7a12ac4fbf40c8612b6d
Mario Daniel C <daniel.cruzbllz@uanl.edu.mx>
2025-05-19 02:48:35 -0600

    Se agrega carpeta pantallas junto con pantalla de menu admin con sus archivos relacionados

---
+++ pantallas/js/madmin.js
@@ -0,0 +1,11 @@
+function verEventos() {
+  window.location.href = "veventos.html";
+}
+
+function gestionarEvento() {
+  window.location.href = "gestevento.html";
+}
+
+function verUsuarios() {
+  window.location.href = "vusuarios.html";
+}
